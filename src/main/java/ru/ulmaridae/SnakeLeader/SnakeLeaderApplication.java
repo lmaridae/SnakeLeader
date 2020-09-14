d6fbf1b6ba1abe4eef44f949f8dab90a317536c3
lmaridae <lmaridae@icloud.com>
2020-09-14 13:24:42 +0300

    initial commit

---
+++ src/main/java/ru/ulmaridae/SnakeLeader/SnakeLeaderApplication.java
@@ -0,0 +1,13 @@
+package ru.ulmaridae.SnakeLeader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnakeLeaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnakeLeaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/ulmaridae/SnakeLeader/SnakeLeaderApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ulmaridae.SnakeLeader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnakeLeaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
